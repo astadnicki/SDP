4583b58faa6dbb3116c923091ffe28e5c0ca6ef5
astadnicki <71561017+astadnicki@users.noreply.github.com>
2023-11-06 16:19:13 -0500

    Delete SDP_Odometry/SDP_Odometry directory

--- SDP_Odometry/SDP_Odometry/movement.h
+++
@@ -1,9 +0,0 @@
-#include <util/delay.h>
-#include <avr/io.h>
-
-int driveForward(float speed);
-int driveRight(float deg, float speed);
-int driveLeft(float deg, float speed);
-//int extend(void);
-//int contract(void);
-//int digCycle(void);
