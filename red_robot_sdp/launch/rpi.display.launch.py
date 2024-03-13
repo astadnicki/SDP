a74341aea4e5abcbc7e71de4f1b9852f5f6563e8
John Boesen <82904895+JohnBoesen12309@users.noreply.github.com>
2024-03-13 00:16:36 -0400

    Add files via upload

--- red_robot_sdp/launch/rpi.display.launch.py
+++ red_robot_sdp/launch/rpi.display.launch.py
@@ -108,34 +108,7 @@ def generate_launch_description():
                     'freq' : 20.0}]
      )
     
-    #SLAM Toolbox
-
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    slam_params_file = LaunchConfiguration('slam_params_file')
-
-    declare_use_sim_time_argument = DeclareLaunchArgument(
-        'use_sim_time',
-        default_value='true',
-        description='Use simulation/Gazebo clock')
-    declare_slam_params_file_cmd = DeclareLaunchArgument(
-        'slam_params_file',
-        default_value=os.path.join(get_package_share_directory("slam_toolbox"),
-                                   'config', 'mapper_params_online_async.yaml'),
-        description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
-
     
-
-    start_async_slam_toolbox_node = Node(
-        parameters=[
-          slam_params_file,
-          {'use_sim_time': use_sim_time}
-        ],
-        package='slam_toolbox',
-        executable='async_slam_toolbox_node',
-        name='slam_toolbox',
-        output='screen')
-        
-        
     #Static Transform
     
     laser_base_tf_node = Node(package = "tf2_ros", executable = "static_transform_publisher", arguments = ["0", "0", "0", "0", "0", "0", "base_link", "laser"])
